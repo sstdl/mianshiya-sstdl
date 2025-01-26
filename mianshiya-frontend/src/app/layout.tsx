932493dec25769604ccbc8adb5891e7bd5aa49d9
SSTDL <2010206234@qq.com>
2025-01-26 13:03:02 +0800

    全局通用布局，请求配置

--- mianshiya-frontend/src/app/layout.tsx
+++ mianshiya-frontend/src/app/layout.tsx
@@ -1,12 +1,26 @@
+"use client";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "./globals.css";
 import BasicLayout from "@/layouts/BasicLayout";
+import { useCallback, useEffect } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  /**
+   * 全局初始化函数，有全局单次调用的代码，都可以写到这里
+   */
+  const doInit = useCallback(() => {
+    console.log("hello 欢迎来到我的项目");
+  }, []);
+
+  // 只执行一次，deps 为空数组，不会改变，只有 deps 改变了才会重新执行 useEffect
+  useEffect(() => {
+    doInit();
+  }, []);
+
   return (
     <html lang="zh">
       <body>
